879ddb3bc0f5c55b2213a4928bb12b73b6b4e637
Sigii1992 <sawlete@gmail.com>
2022-02-23 12:52:52 +0000

    Create Browser Router with Routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar'
 import './App.css';
 
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-        <div className="App">
-          nesamone
-        </div>
+      <div className="App">
+        <Container>
+          <Routes>
+            <Route path='/' element={<Trending/>} exact />
+            <Route path='/movies' element={<Movies/>} />
+            <Route path='/tv-series' element={<TVseries/>} />
+            <Route path='/search' element={<Search/>} />
+          </Routes>
+        </Container>  
+      </div>
       <Navbar />
-    </>
+    </BrowserRouter>
   );
 }
 
