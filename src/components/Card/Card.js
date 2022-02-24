077d872234a1a75d7e54bac36e5f0e61585f1568
Sigii1992 <sawlete@gmail.com>
2022-02-24 13:13:22 +0000

    Add a vote average badge at the top of the card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,11 @@
 import { img_300, unavailable } from '../../config/config';
+import Badge from '@material-ui/core/Badge';
 import './Card.css'
 
 const Card = (props) => {
   return (
     <div className="card">
+      <Badge badgeContent={props.vote_average} color={props.vote_average > 6 ? "secondary" : "primary"}></Badge>
       <img className="card--image" src={`${img_300}${props.poster_path}` || unavailable} />
       <b className="card--title">{props.title || props.name}</b>
       <span className="card--type--date">{props.media_type === "movie" ? "Movie" : "TV series"}
