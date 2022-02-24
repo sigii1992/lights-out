9d829b1375ab5457572d4bfeff7cd29b3a833074
Sigii1992 <sawlete@gmail.com>
2022-02-24 14:13:07 +0000

    Add Pagination at the bottom of the page

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,7 +5,7 @@ import './Card.css'
 const Card = (props) => {
   return (
     <div className="card">
-      <Badge badgeContent={props.vote_average} color={props.vote_average > 6 ? "secondary" : "primary"}></Badge>
+      <Badge badgeContent={props.vote_average} color={props.vote_average > 6 ? "primary" : "secondary"}></Badge>
       <img className="card--image" src={`${img_300}${props.poster_path}` || unavailable} />
       <b className="card--title">{props.title || props.name}</b>
       <span className="card--type--date">{props.media_type === "movie" ? "Movie" : "TV series"}
--- src/components/Pages/Trending/Trending.js
+++ src/components/Pages/Trending/Trending.js
@@ -1,15 +1,17 @@
 import { useState, useEffect } from 'react';
 import Card from '../../Card/Card'
+import CustomPagination from '../../Header/Pagination/CustomPagination';
 import './Trending.css'
 
 const Trending = () => {
+  const [page, setPage] = useState(1)
   const [trendingContent, setTrendingContent] = useState([])
   
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`)
+    fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`)
     .then(response => response.json())
     .then(data => setTrendingContent(data.results))
-  },[])
+  },[page])
 
   console.log(trendingContent)
 
@@ -26,6 +28,7 @@ const Trending = () => {
       <div className="trending">
         {trendingContent && cards}
       </div>
+      <CustomPagination setPage={setPage}/>
     </div>
   )
 }
