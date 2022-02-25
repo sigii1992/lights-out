fa64697bf16549443a28a25421d05735ef0e71d8
Sigii1992 <sawlete@gmail.com>
2022-02-25 13:07:23 +0000

    User can select and remove selected genre

--- src/components/Genres.js
+++ src/components/Genres.js
@@ -3,6 +3,18 @@ import Chip from '@material-ui/core/Chip';
 
 const Genres = ({type, genres, setGenres, selectedGenres, setSelectedGenres, setPage}) => {
 
+  const handleAdd = (genre) => {
+    setSelectedGenres([...selectedGenres, genre]);
+    setGenres(genres.filter((g) => g.id !== genre.id));
+    setPage(1);
+  }
+
+  const handleRemove = (genre) => {
+    setSelectedGenres(selectedGenres.filter((selected) => selected.id !== genre.id));
+    setGenres([...genres, genre]);
+    setPage(1);
+  }
+
   useEffect(() => {
     let mounted = true;
     fetch(`https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
@@ -12,9 +24,20 @@ const Genres = ({type, genres, setGenres, selectedGenres, setSelectedGenres, set
         setGenres(data.genres)}
       })
     return () => mounted = false;
-  },[type, setGenres])
+  },[type])
 
   console.log(genres)
+
+  const selectedChip = selectedGenres?.map(genre => {
+    return <Chip 
+      key={genre.id}
+      label={genre.name}
+      style={{ margin: 3 }}
+      clickable
+      color="secondary"
+      onDelete={() => handleRemove(genre)}
+    />
+  })
 
   const genreChip = genres?.map(genre => {
     return <Chip 
@@ -23,11 +46,14 @@ const Genres = ({type, genres, setGenres, selectedGenres, setSelectedGenres, set
       style={{ margin: 3 }}
       clickable
       color="primary"
+      onClick={() => handleAdd(genre)}
+      
     />
   })
 
   return (
     <div style={{ padding: "6px 0" }}>
+      {selectedChip}
       {genreChip}
     </div>
   )
