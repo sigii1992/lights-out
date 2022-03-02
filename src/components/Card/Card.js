d8aa7dad9bc190b0f383def9748ca0404c5a848c
Sigii1992 <sawlete@gmail.com>
2022-03-02 12:59:41 +0000

    Create ContentModal component using material ui, fetch data from API, display image in modal

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,17 +1,18 @@
 import { img_300, unavailable } from '../../config/config';
 import Badge from '@material-ui/core/Badge';
 import './Card.css'
+import ContentModal from '../ContentModal/ContentModal';
 
 const Card = (props) => {
   return (
-    <div className="card">
+    <ContentModal media_type={props.media_type} id={props.id}>
       <Badge badgeContent={props.vote_average ? props.vote_average : '?'} color={props.vote_average > 6 ? "primary" : "secondary"}></Badge>
       <img className="card--image" alt='' src={props.poster_path ? `${img_300}${props.poster_path}` : unavailable} />
       <b className="card--title">{props.title || props.name}</b>
       <span className="card--type--date">{props.media_type === "movie" ? "Movie" : "TV series"}
         <span>{props.release_date || props.first_air_date ? props.release_date || props.first_air_date : "Unknown date"}</span>
       </span>
-    </div>
+    </ContentModal>
   )
 }
 
